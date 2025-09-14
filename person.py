800b086252179286a99387d8ba7a16265cedf121
TLAllen662 <tlallen1419@gmail.com>
2025-09-14 17:07:46 -0500

    Implement Person class with initialization, voting eligibility, and string representation

--- person.py
+++ person.py
@@ -1,3 +1,17 @@
 class Person:
     # Delete pass and write your code here
-    pass
+    def __init__ (self, name, age, country):
+        self.name = name
+        self.age = age
+        self.country = country
+    
+    def can_vote (self):
+        return self.age >= 18
+    
+
+    def __str__ (self):
+        return f"{self.name}, is {self.age} years old and lives in {self.country}."
+person_1 = Person("Alice", 30, "USA")
+print(person_1)
+person_2 = Person("Bob", 17, "Canada")
+print(person_2)
